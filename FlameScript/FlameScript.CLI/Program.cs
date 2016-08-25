f50b6352884b735d5b9b9eb8c070a7586acf07b1
0xFireball <0xFireball@outlook.com>
2016-08-25 15:01:21 -0700

    create project

---
+++ FlameScript/FlameScript.CLI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlameScript.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No input file specified");
+                return;
+            }
+            var scriptPath = args[0];
+
+        }
+    }
+}
---
+++ FlameScript/FlameScript/Lexing/CharType.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace FlameScript.Lexing
+{
+    /// <summary>
+    /// The possible classes for a character.
+    /// This is a Flags-enum, please note that there are
+    /// compund values.
+    /// </summary>
+    [Flags]
+    internal enum CharType
+    {
+        /// <summary>
+        /// Unknown. This is 0x00, so it can't be checked with HasFlag!
+        /// (A character can only be unknown or anything else)
+        /// </summary>
+        Unknown = 0x00,
+
+        /// <summary>
+        /// a-z,A-Z,_. Anything acceptable to start an identifier.
+        /// </summary>
+        Alpha = 0x01, //we need binary literals!
+
+        /// <summary>
+        /// 0-9.
+        /// </summary>
+        Numeric = 0x02,
+
+        /// <summary>
+        /// Spaces,tabs. Whitespace, but no newline.
+        /// </summary>
+        LineSpace = 0x04,
+
+        /// <summary>
+        /// A newline character.
+        /// </summary>
+        NewLine = 0x08,
+
+        /// <summary>
+        /// +,-,*,/,%,&,|,=,&gt;,&lt;,!.
+        /// </summary>
+        Operator = 0x10,
+
+        /// <summary>
+        /// (,[,{.
+        /// </summary>
+        OpenBrace = 0x20,
+
+        /// <summary>
+        /// ),],}.
+        /// </summary>
+        CloseBrace = 0x40,
+
+        /// <summary>
+        /// ,. Comma used to seperate arguments to functions.
+        /// </summary>
+        ArgSeperator = 0x80,
+
+        /// <summary>
+        /// ;. Semicolon used to seperate statements.
+        /// </summary>
+        StatementSeperator = 0x100,
+
+        //compund values:
+        AlphaNumeric = Alpha | Numeric,
+
+        WhiteSpace = LineSpace | NewLine,
+        Brace = OpenBrace | CloseBrace,
+
+        /// <summary>
+        /// Chars that "have a special meaning".
+        /// </summary>
+        MetaChar = Operator | Brace | ArgSeperator | StatementSeperator,
+
+        All = AlphaNumeric | WhiteSpace | MetaChar,
+    }
+}

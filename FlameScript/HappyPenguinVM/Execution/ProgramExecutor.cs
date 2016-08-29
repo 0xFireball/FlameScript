0e90f063eb3a41faa7bd05273b903acc38a71c79
0xFireball <0xFireball@outlook.com>
2016-08-29 12:47:00 -0700

    add executor stub

---
+++ FlameScript/HappyPenguinVM/Execution/ProgramExecutor.cs
@@ -0,0 +1,20 @@
+﻿using HappyPenguinVM.Types;
+
+namespace HappyPenguinVM.Execution
+{
+    public class ProgramExecutor
+    {
+        private const int DEFAULT_MEMORY_SIZE = 1024; //1K cells, or 4KiB memory
+
+        private int[] memory; //one memory cell is sizeof(int) big
+
+        private int programCounter;
+        private int stackPointer;
+        private int heapPointer;
+        private int framePointer;
+        private int extremePointer; //TODO: Needed?
+        private HappyPenguinVMProgram code;
+
+
+    }
+}

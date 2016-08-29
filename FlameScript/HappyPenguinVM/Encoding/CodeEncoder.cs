02f0954d0a1ca6f79a0d761fc21cedb26d21685a
0xFireball <0xFireball@outlook.com>
2016-08-28 23:25:27 -0700

    read unmanaged instruction memory from stream

--- FlameScript/HappyPenguinVM/Encoding/CodeEncoder.cs
+++ FlameScript/HappyPenguinVM/Encoding/CodeEncoder.cs
@@ -1,5 +1,7 @@
 ﻿using HappyPenguinVM.Types;
+using System;
 using System.IO;
+using System.Linq;
 
 namespace HappyPenguinVM.Encoding
 {
@@ -8,16 +10,16 @@ namespace HappyPenguinVM.Encoding
     /// </summary>
     public class CodeEncoder
     {
-        public static string MagicHeader { get; } = "☺🐧☺";
+        public static byte[] MagicHeader { get; } = new byte[] { 0xF0, 0x9F, 0x90, 0xA7 }; //"🐧"
 
         public void EncodeCodeToStream(HappyPenguinVMProgram programCode, Stream outputStream)
         {
-            var outputStreamWriter = new StreamWriter(outputStream);
+            var outputStreamWriter = new BinaryWriter(outputStream);
             //Write magic header
             outputStreamWriter.Write(MagicHeader);
             foreach (var instruction in programCode)
             {
-                outputStreamWriter.Write(instruction.OpCode);
+                outputStreamWriter.Write((int)instruction.OpCode);
                 outputStreamWriter.Write(instruction.ByteArg1);
                 outputStreamWriter.Write(instruction.ByteArg2);
             }
@@ -26,26 +28,30 @@ namespace HappyPenguinVM.Encoding
 
         public HappyPenguinVMProgram ReadCodeFromStream(Stream inputStream)
         {
-            var inputStreamReader = new StreamReader(inputStream);
+            var inputStreamReader = new BinaryReader(inputStream);
             //var inputData = inputStreamReader.ReadToEnd();
 
             //Verify header
-            char[] headerBuffer = new char[MagicHeader.Length];
-            inputStreamReader.ReadBlock(headerBuffer, 0, headerBuffer.Length);
-            var readHeaderString = new string(headerBuffer);
-            var headerMatch = readHeaderString == MagicHeader;
+            byte[] headerBuffer = new byte[MagicHeader.Length];
+            headerBuffer = inputStreamReader.ReadBytes(headerBuffer.Length);
+            var headerMatch = MagicHeader.SequenceEqual(headerBuffer);
             if (!headerMatch)
             {
                 throw new InvalidDataException("The data does not contain a valid HappyPenguinVM program!");
             }
             //Header matches, now read instructions into code
             var readProgram = new HappyPenguinVMProgram();
-            while (!inputStreamReader.EndOfStream)
+            byte[] instructionReaderBuffer = new byte[sizeof(int) + sizeof(short)]; //Size of the instruction (int + short)
+            while (inputStreamReader.BaseStream.Position < inputStreamReader.BaseStream.Length)
             {
-                inputStreamReader.Read();
+                inputStreamReader.Read(instructionReaderBuffer, 0, instructionReaderBuffer.Length);
+                int opCodeInt = BitConverter.ToInt32(instructionReaderBuffer, 0);
+                byte arg1 = instructionReaderBuffer[4]; //Get the fifth byte
+                byte arg2 = instructionReaderBuffer[5]; //Get the sixth byte
+                readProgram.Add(new CodeInstruction { OpCode = (OpCode)opCodeInt, ByteArg1 = arg1, ByteArg2 = arg2 });
             }
 
-            return new HappyPenguinVMProgram();
+            return readProgram;
         }
     }
 }

753cfe92a9b86385a1cf6a6296d4d81a279ab3b6
0xFireball <0xFireball@outlook.com>
2016-09-01 21:33:07 -0700

    copy static assets

---
+++ FlameScript/FlameScript.Web/Modules/CodeProcessorModule.cs
@@ -0,0 +1,23 @@
+﻿using Nancy;
+using System.IO;
+
+namespace FlameScript.Web.Modules
+{
+    public class CodeProcessorModule : NancyModule
+    {
+        public CodeProcessorModule()
+        {
+            Get("/", _ =>
+            {
+                return View["Editor"];
+            });
+            Post("/processCode", (parameters) =>
+            {
+                var postDataStream = Request.Body;
+                var postDataReader = new StreamReader(postDataStream);
+                var rawPostData = postDataReader.ReadToEnd();
+                return "Hello, World! 2.0";
+            });
+        }
+    }
+}

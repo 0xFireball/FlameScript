bbd46653f23511404873aeffe4badd69f64c8b3a
0xFireball <0xFireball@outlook.com>
2016-09-01 21:03:55 -0700

    add web project

---
+++ FlameScript/FlameScript.Web/Program.cs
@@ -0,0 +1,22 @@
+﻿using Nancy.Hosting.Self;
+using System;
+
+namespace FlameScript.Web
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var port = 4077;
+            var nancyHostUri = new Uri($"http://localhost:{port}");
+            var hostConf = new HostConfiguration { RewriteLocalhost = false };
+
+            using (var host = new NancyHost(hostConf, nancyHostUri))
+            {
+                host.Start();
+                Console.WriteLine($"Application server running on {nancyHostUri}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
